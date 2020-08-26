febb1186cfe663df3eb36906e62c7a2548876bb2
Katie Poa <kpoa@berkeley.edu>
2020-08-25 18:45:56 -0700

    Fix this.$node definition (no need for span)

--- src/dancer.js
+++ src/dancer.js
@@ -1,15 +1,15 @@
 // Creates and returns a new dancer object that can step
-var Dancer = function(top, left, timeBetweenSteps) {
+var Dancer = function (top, left, timeBetweenSteps) {
   //this = Object.create(Dancer.prototype);
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
-  this.$node = $('<span class="dancer"></span>');
+  this.$node = $('');
 
   //return this;
 };
 
-Dancer.prototype.step = function() {
+Dancer.prototype.step = function () {
   setTimeout(() => { return this.step(); }, this.timeBetweenSteps); // arrow function cancels the assignment of this keyword to wrapper function
 };
 
@@ -21,7 +21,7 @@ newDancer.step();
 this.step(); */
 // ^call from BlinkyDancer instance of Dancer
 
-Dancer.prototype.setPosition = function(top, left) {
+Dancer.prototype.setPosition = function (top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
